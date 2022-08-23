82b76e6e8fa922aef9d185568edec8ef32c3da2a
Yan Bein <rbeinyan@gmail.com>
2022-08-23 08:45:50 -0300

    Fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('sms-menu-bundle');
 
         $treeBuilder->root('cmf_menu')
             ->fixXmlConfig('voter')
